c2986ea81ee253f1c7d54fa397b57fc6ea6be104
mendezwin <mendezwin@gmail.com>
2016-07-14 20:28:10 +0000

    Better styling, transitions, etc.

--- colorGame.js
+++ colorGame.js
@@ -50,7 +50,7 @@ resetButton.addEventListener("click", function(){
         squares[i].style.background = colors[i];
     }
     
-    h1.style.background = "#232323";
+    h1.style.background = steelblue;
     
 })
 
